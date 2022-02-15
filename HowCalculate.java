f4c36b17a3ac66cd40f3695fe6ca0c29bfa707e0
Darya-QA-it <94610351+Darya-QA-it@users.noreply.github.com>
2022-02-15 21:16:44 +0300

    Add files via upload

---
+++ HowCalculate.java
@@ -0,0 +1,14 @@
+public class HowCalculate {
+    public static void main(String[] args) {
+        int n=5%3;
+        System.out.println("n="+n);
+        double m=3./0*0;
+        System.out.println(m);
+        m=Math.sin(Math.PI/6);
+            System.out.println(m);
+
+            float n2=6.0f;
+            System.out.println(n2);
+
+    }
+}
---
+++ branchet.java
@@ -0,0 +1,11 @@
+public class branchet {
+    public static void main(String[] args) {
+        int points = 20;
+        if (points < 30)
+            System.out.println("Your mark is 3");
+        else if (points < 50)
+            System.out.println("Your mark is 4");
+        else
+            System.out.println("Your mark is 5");
+    }
+}
